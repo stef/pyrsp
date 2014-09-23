3464660ef3d9eb3b58ce70b4906266ff02e46f72
Simeon Visser <svisser@users.noreply.github.com>
2014-09-23 01:04:28 +0100

    Ensure file handle is closed using with statement

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from setuptools import setup
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)):
+        return f.read()
 
 setup(
     name = "pyrsp",
