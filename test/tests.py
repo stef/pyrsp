1d46ff4e01cc308734538538dec0a4e1f60faed5
Efimov Vasily <real@ispras.ru>
2018-12-12 20:39:20 +0300

    unittest: QEMU based test for CortexM3
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,7 @@
 from platform import machine
 from unittest import main, TestCase
 from subprocess import Popen
+from struct import pack
 
 from os.path import split, join
 
@@ -10,8 +11,8 @@ parent_dir = split(test_dir)[0]
 # adjust PYTHONPATH to import pyrsp
 from sys import path as PYTHONPATH
 PYTHONPATH.insert(0, parent_dir)
-from pyrsp.rsp import archmap
-from pyrsp.utils import find_free_port, wait_for_tcp_port
+from pyrsp.rsp import archmap, CortexM3
+from pyrsp.utils import find_free_port, wait_for_tcp_port, QMP
 
 
 def run(*args, **kw):
@@ -62,6 +63,61 @@ class TestUserSimple(TestUser):
     def test_simple(self):
         self._target.run(setpc=False)
 
+
+class TestARM(TestCase):
+
+    def setUp(self):
+        self.example_dir = join(parent_dir, "example")
+        self.elf_path = join(self.example_dir, "test.elf")
+
+        self.assertEqual(run("make", cwd = self.example_dir), 0)
+        rsp_port = find_free_port()
+        qmp_port = find_free_port(rsp_port + 1)
+        if (rsp_port or qmp_port) is None:
+            raise RuntimeError("Cannot find free port!")
+        self._port = str(rsp_port)
+        qargs = [
+            "qemu-system-arm",
+            "-machine", "netduino2",
+            "-kernel", self.elf_path,
+            "-S", # guest is initially stopped
+            "-gdb", "tcp:localhost:" + self._port,
+            # QEMU monitor protocol for VM management
+            "-qmp", "tcp:localhost:%u,server,nowait" % qmp_port,
+            # do not create a window
+            "-nographic"
+        ]
+        print(" ".join(qargs))
+        self._qemu = qemu = Popen(qargs)
+
+        if (not wait_for_tcp_port(qmp_port)
+            or not wait_for_tcp_port(rsp_port)
+            or qemu.returncode is not None
+        ):
+            raise RuntimeError("QEMU malfunction")
+
+        self.qmp = QMP(qmp_port)
+
+        self._target = CortexM3(self._port,
+            elffile = self.elf_path,
+            verbose = True
+        )
+
+
+    def test_example(self):
+        target = self._target
+
+        # Initialize reset vector to entry point address (with 0-th bit set)
+        # and reset the board. It will switch the CPU to Thumb mode.
+        target[4] = pack("<I", target.elf.entry)
+        self.qmp("system_reset")
+
+        target.set_br("rsp_finish", target.finish_cb)
+        target.set_br("rsp_dump", target.dump_cb)
+        target.run()
+
+    def tearDown(self):
+        self._qemu.terminate()
 
 if __name__ == "__main__":
     main()
