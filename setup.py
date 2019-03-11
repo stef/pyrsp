34d1aff7620d6ba2cb8f87590a0d1c9cb2b04493
stf <7o5rfu92t@ctrlc.hu>
2019-03-11 16:20:21 +0100

    [enh] bumped version to 0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name = "pyrsp",
-    version = "0.3.2",
+    version = "0.4",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     description = ("simple GDB remote serial protocol wrapper"),
